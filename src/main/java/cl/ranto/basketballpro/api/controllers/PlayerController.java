8b850e2d98b7bca7097949e37ede17f8c7cca66d
Juan Francisco Maldonado Leon <juan.maldonado.leon@gmail.com>
2021-12-05 02:45:04 -0300

    players api

--- src/main/java/cl/ranto/basketballpro/api/controllers/PlayerController.java
+++ src/main/java/cl/ranto/basketballpro/api/controllers/PlayerController.java
@@ -2,11 +2,12 @@ package cl.ranto.basketballpro.api.controllers;
 
 import cl.ranto.basketballpro.api.core.Player;
 import cl.ranto.basketballpro.api.services.PlayerService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 
 
@@ -22,20 +23,35 @@ public class PlayerController {
     @Autowired
     private PlayerService service;
 
-    @RequestMapping(method = RequestMethod.GET)
+    private final static Logger LOGGER = LoggerFactory.getLogger(PlayerController.class);
+
+    @GetMapping
     public Flux<Player> listAll() {
         return service.listAll();
     }
 
-    @RequestMapping(method = RequestMethod.GET, value="/{oid}")
+    @GetMapping("/{oid}")
     public Player findById( @PathVariable("oid") String oid ){
         return service.findById(oid);
     }
 
-
-    @RequestMapping(method = RequestMethod.DELETE, value="/{oid}")
+    @DeleteMapping("/{oid}")
     public void deteleById( @PathVariable("oid") String oid ){
         service.deleteById(oid);
+    }
+
+    @PostMapping
+    public ResponseEntity<Player> update( @RequestBody Player player){
+
+        try{
+            service.update(player);
+            return new ResponseEntity<>(
+                    HttpStatus.OK);
+        }catch (Exception ex){
+            LOGGER.error(ex.getMessage(), ex);
+            return new ResponseEntity<>(
+                    HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
 }
--- src/main/java/cl/ranto/basketballpro/api/services/PlayerService.java
+++ src/main/java/cl/ranto/basketballpro/api/services/PlayerService.java
@@ -1,10 +1,21 @@
 package cl.ranto.basketballpro.api.services;
 
+import cl.ranto.basketballpro.api.core.GameState;
 import cl.ranto.basketballpro.api.core.Player;
+import cl.ranto.basketballpro.api.core.exceptions.ServicesException;
 import cl.ranto.basketballpro.api.repositories.PlayerRepository;
+import cl.ranto.basketballpro.api.utils.Constants;
+import com.google.api.core.ApiFuture;
+import com.google.cloud.firestore.DocumentReference;
+import com.google.cloud.firestore.Firestore;
+import com.google.cloud.firestore.WriteResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
 
 @Service
 public class PlayerService {
@@ -12,6 +23,9 @@ public class PlayerService {
 
     @Autowired
     private PlayerRepository repository;
+
+    @Autowired
+    private Firestore firestore;
 
     public Flux<Player> listAll(){
         return repository.findAll();
@@ -23,11 +37,32 @@ public class PlayerService {
 
 
     public void deleteById( String oid ){
-        repository.deleteById(oid);
+        repository.deleteById(oid).block();
     }
 
     public void save(Player player){
-        repository.save(player);
+        repository.save(player).block();
     }
+
+    public void update(Player player) throws ServicesException {
+        try{
+            DocumentReference playerRef = this.firestore.collection( Constants.COLLECTION_PLAYERS ).document( player.getOid() );
+            Map<String, Object> data = new HashMap<>();
+            data.put("number", player.getNumber() );
+            data.put("position", player.getPosition().toString());
+            data.put("name", player.getName());
+            data.put("lastName", player.getLastName());
+            data.put("birthdate", player.getBirthdate());
+            data.put("height", player.getHeight());
+            data.put("weight", player.getWeight());
+            data.put("gender", player.getGender().toString());
+            ApiFuture<WriteResult> writeResult = playerRef.update( data);
+            writeResult.get().getUpdateTime();
+        } catch (InterruptedException | ExecutionException e) {
+            Thread.currentThread().interrupt();
+            throw new ServicesException( "Ocurrio un error al guardar la informacion", e );
+        }
+    }
+
 
 }
